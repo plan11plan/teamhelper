e3dd71d6799fe4e1270b3eb105af23c35bd2eaa5
plan11plan <tjtfy7028@gmail.com>
2025-01-11 15:18:25 +0900

    initialize commit

---
+++ src/main/java/com/teamhelper/TeamhelperApplication.java
@@ -0,0 +1,13 @@
+package com.teamhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamhelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamhelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamhelper/TeamhelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamhelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
